4df232e826878b06c141c836f4a751291b198f30
Razi Montgomery <darkwolfrazi@yahoo.com>
2020-01-29 21:28:57 -0800

    completed the triangle coding task

--- script1.js
+++ script1.js
@@ -8,6 +8,19 @@
 // ##
 // #
  
- for (let i = 1; i<=7; i++) {
-     console.log (i);
+ for (let i = 1; i<=4; i++) {
+     let R=("");
+     for (let T = 1; T<=i; T++) {
+         R += "#";
+         
+     }console.log (R);
+ } 
+ 
+
+ for (let j =3; j>=1; j--){
+    let R=("");
+    for (let T = 1; T<=j; T++) {
+        R += "#";
+        
+    }console.log (R);
  }
